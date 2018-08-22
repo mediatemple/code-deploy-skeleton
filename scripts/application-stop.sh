7dfedcaa6467d5dc3b1794148f8f7397c00b30ea
Adam Joseph <ajoseph@mediatemple.net>
2018-08-22 12:35:55 -0700

    Add skel and reorg files

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$DEPLOYMENT_GROUP_NAME" == "Prod" ]
 then
-	aws elb deregister-instances-from-load-balancer --load-balancer-name PRODUCTION-LB-NAME --instances $(curl http://169.254.169.254/latest/meta-data/instance-id) --region=us-west-2 
+	aws elb deregister-instances-from-load-balancer --load-balancer-name lb-mng-svcs-cd-prod-nlb-web-7e71b71fed8b3c9c.elb.us-west-2.amazonaws.com --instances $(curl http://169.254.169.254/latest/meta-data/instance-id) --region=us-west-2 
 fi
 
 sleep 30 
